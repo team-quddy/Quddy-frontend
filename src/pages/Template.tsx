725ffb9392df42ddf87cb25d7367d7445a0a97b6
ygo65312 <ygo65312@naver.com>
2023-09-09 05:23:47 +0900

    🎨 update Templae page

--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -5,6 +5,9 @@ import { ExamTemplateType } from "../types/types";
 import { getExamTemplateList } from "../apis/Setter";
 import SearchInput from "../components/common/Search/SearchInput";
 import SearchSorter from "../components/common/Search/SearchSorter";
+import Footer from "../components/common/Footer/Footer";
+import Loading from "../components/common/Loading/Loading";
+import ExamTemplateItem from "../components/common/ExamTemplateItem/ExamTemplateItem";
 
 const Template = () => {
   const [option, setOption, query] = useSearch<ExamTemplateType[]>(getExamTemplateList);
@@ -12,36 +15,85 @@ const Template = () => {
 
   return (
     <TemplateComponent>
-      <img className="guide-banner" src={GuideBanner} alt="큐디 완벽 가이드 배너" />
-      <section>
-        <SearchInput search={text} setSearch={(text) => setOption((pre) => ({ ...pre, text }))} placeholder="asdf" />
+      <div>
+        <img className="guide-banner" src={GuideBanner} alt="큐디 완벽 가이드 배너" />
+        <section>
+          <SearchInput search={text} setSearch={(text) => setOption((pre) => ({ ...pre, text }))} placeholder="asdf" />
 
-        <div className="header">
-          <h1>문제집 템플릿</h1>
-          <SearchSorter option={sort} setOption={(sort) => setOption((pre) => ({ ...pre, sort }))} />
-        </div>
-      </section>
+          {/* 헤더 */}
+          <div className="header">
+            <h1>문제집 템플릿</h1>
+            <SearchSorter option={sort} setOption={(sort) => setOption((pre) => ({ ...pre, sort }))} />
+          </div>
+
+          {/* 템플릿 목록 */}
+          <div className="list">
+            {query.data?.map((item) => (
+              <ExamTemplateItem key={item.id} exam={item} />
+            ))}
+          </div>
+
+          {/* 로딩 컴포넌트 */}
+          {query.status === "loading" ? <Loading /> : undefined}
+        </section>
+      </div>
+      <Footer />
     </TemplateComponent>
   );
 };
 
 const TemplateComponent = styled.div`
+  width: 100%;
+  max-width: 800px;
+  min-height: calc(100vh - 48px);
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
   & .guide-banner {
     width: 100%;
   }
-  & > section {
-    padding: 12px 20px;
+  & .list {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-column-gap: 10px;
+    grid-row-gap: 16px;
+
+    & > div {
+      width: 100%;
+    }
+
+    @media (max-width: 800px) {
+      grid-template-columns: repeat(4, 1fr);
+    }
+    @media (max-width: 480px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+    @media (max-width: 344px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media (max-width: 266px) {
+      grid-template-columns: 1fr;
+    }
   }
-  & h1 {
-    font-size: 20px;
-    font-weight: bold;
-  }
-  & .header {
-    margin-top: 14px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  & section {
+    margin: 12px 8px 20px;
+
+    & h1 {
+      font-size: 20px;
+      font-weight: bold;
+    }
+    & > *:first-child {
+      margin: 0 4px;
+    }
+    & .header {
+      padding: 14px 12px 20px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
   }
 `;
 
