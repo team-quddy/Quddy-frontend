b7619f0f057741bc41e75c12acba1290d66370e8
ygo65312 <ygo65312@naver.com>
2023-09-09 05:23:14 +0900

    🧪 update test code : loading

--- src/apis/Setter.ts
+++ src/apis/Setter.ts
@@ -4,10 +4,16 @@ import getInstance from ".";
 import { SearchOption } from "../types/search";
 import { ExamTemplateType } from "../types/types";
 
+// sample data
+import SampleExamList from "./sample/Exam.json";
+
 /**
  * [GET] 문제집 템플릿 목록 요청
  * @param option 검색 옵션
  */
-export function getExamTemplateList(searchOption: SearchOption): Promise<ExamTemplateType[]> {
-  return getInstance().get("/template", { params: searchOption });
+export async function getExamTemplateList(searchOption: SearchOption): Promise<ExamTemplateType[]> {
+  // TODO : 임시 이벤트이므로 추후 api 명세에 따라 수정 필요
+  await new Promise((res) => setTimeout(res, 200));
+  return SampleExamList;
+  return await getInstance().get("/template", { params: searchOption });
 }
