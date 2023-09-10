939bceb8a9c5228fbd5324f868089d3d9886b20d
한유경 <ygo65312@naver.com>
2023-09-10 17:28:07 +0900

    🎨 update option initial status

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -9,12 +9,12 @@ const useSearch = <T>(
     text: "",
     target: SearchTarget.title,
     sort: Sort.popular,
-    page: 0,
-    size: 6,
+    page: 1,
+    size: 30,
   });
 
   // TODO : 과도한 재검색을 방지하기위한 이벤트 제어 추가
-  const query = useQuery({ queryKey: ["searchData", option], queryFn: () => func(option) });
+  const query = useQuery(["searchData", option], () => func(option));
 
   return [option, setOption, query];
 };
