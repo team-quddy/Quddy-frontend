d0c105825a3b00f48119a218a71bfa2fe4676b3a
한유경 <ygo65312@naver.com>
2023-09-10 17:26:53 +0900

    🧪 update sample api code

--- src/apis/Setter.ts
+++ src/apis/Setter.ts
@@ -1,6 +1,7 @@
 // 출제자용 요청 모음
 
 import getInstance from ".";
+import { ResponseListType } from "../types/response";
 import { SearchOption } from "../types/search";
 import { ExamTemplateType } from "../types/types";
 
@@ -11,9 +12,12 @@ import SampleExamList from "./sample/Exam.json";
  * [GET] 문제집 템플릿 목록 요청
  * @param option 검색 옵션
  */
-export async function getExamTemplateList(searchOption: SearchOption): Promise<ExamTemplateType[]> {
+export async function getExamTemplateList(searchOption: SearchOption): Promise<ResponseListType<ExamTemplateType>> {
   // TODO : 임시 이벤트이므로 추후 api 명세에 따라 수정 필요
-  await new Promise((res) => setTimeout(res, 200));
-  return SampleExamList;
+  await new Promise((res) => setTimeout(res, 500));
+  return {
+    list: SampleExamList.map((item) => ({ ...item, id: `${item.id}${searchOption.page}` })),
+    page: searchOption.page,
+  };
   return await getInstance().get("/template", { params: searchOption });
 }
