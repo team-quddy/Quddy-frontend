bde9b9c987bb3ee337b273a6c05f2b5e8bd1fc81
한유경 <ygo65312@naver.com>
2023-09-14 16:41:35 +0900

    🎨 update accordion event : add custom event for resizing inner component

--- src/components/common/Accordion/Accordion.tsx
+++ src/components/common/Accordion/Accordion.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { TbChevronDown } from "react-icons/tb";
 import { styled } from "styled-components";
+import { StretchHeightEvent } from "../../../types/event";
 
 interface Props {
   className?: string;
@@ -15,6 +16,19 @@ const Accordion = ({ className = "", children }: Props) => {
     if (!divRef.current) return;
     if (open) divRef.current.style.height = divRef.current.scrollHeight + "px";
     else divRef.current.style.height = "40px";
+
+    const target = divRef.current;
+
+    function onChangeHeight(this: HTMLElement, e: CustomEvent<StretchHeightEvent>) {
+      console.dir(e);
+      this.style.height = this.scrollHeight + e.detail.height + "px";
+    }
+
+    target.addEventListener("stretchHeight", onChangeHeight);
+
+    return () => {
+      target.removeEventListener("stretchHeight", onChangeHeight);
+    };
   }, [open]);
 
   const onToggle = () => setOpen((pre) => !pre);
