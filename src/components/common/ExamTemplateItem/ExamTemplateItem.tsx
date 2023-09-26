83532eb7ed2af441d6d19be6c5326820d541a573
한유경 <ygo65312@naver.com>
2023-09-26 18:07:44 +0900

    🚚 move func : num2str

--- src/components/common/ExamTemplateItem/ExamTemplateItem.tsx
+++ src/components/common/ExamTemplateItem/ExamTemplateItem.tsx
@@ -1,21 +1,13 @@
 import { styled } from "styled-components";
 import { ExamTemplateType } from "../../../types/types";
 import { TbGitFork } from "react-icons/tb";
+import { num2str } from "../../../utils/string";
 
 interface Props {
   exam: ExamTemplateType;
 }
 
 const ExamTemplateItem = ({ exam }: Props) => {
-  /** 숫자 단위에 맞게 문자열로 변경하는 함수 */
-  const num2str = (n: number): string => {
-    if (n < 1000) return `${n}`;
-    if (n < 10000) return `${(n / 1000).toFixed(1)} K`;
-    if (n < 1000000) return `${(n / 1000).toFixed(0)} K`;
-    if (n < 10000000) return `${(n / 1000000).toFixed(1)} M`;
-    else return `${(n / 1000000).toFixed(0)} M`;
-  };
-
   return (
     <ExamTemplateItemComponent>
       <div>
